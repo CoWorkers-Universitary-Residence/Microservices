7f18d10e7ce1eae30e0e7b6630a4a39cc69e7433
Alejandro Jacobo <alejandro.jacobo@outlook.es>
2022-10-22 16:04:09 -0500

    fix: eureka port publication service

--- rental-service/src/main/java/pe/edu/rental/rentalservice/RentalServiceApplication.java
+++ rental-service/src/main/java/pe/edu/rental/rentalservice/RentalServiceApplication.java
@@ -2,11 +2,13 @@ package pe.edu.rental.rentalservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaAuditing
 public class RentalServiceApplication {
