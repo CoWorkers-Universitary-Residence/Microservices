7d4c9f0f58191d60ea61cbb275cd365681e00ebf
JesussPacheco <jesuspacheco0219@gmail.com>
2022-10-23 18:38:36 -0500

    add persistance

---
+++ subscription-service/src/main/java/com/example/subscriptionservice/Domain/persistance/SubscriptionRepository.java
@@ -0,0 +1,10 @@
+package com.example.subscriptionservice.Domain.persistance;
+
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SubscriptionRepository {
+}
