33da64ea4f76b6116578a2d19dec8433a5b486db
JesussPacheco <jesuspacheco0219@gmail.com>
2022-10-23 18:23:48 -0500

    add Domain

---
+++ subscription-service/src/main/java/com/example/subscriptionservice/Domain/entity/Subscription.java
@@ -0,0 +1,32 @@
+package com.example.subscriptionservice.Domain.entity;
+
+
+import com.example.subscriptionservice.Domain.enums.Type;
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@With
+@Entity
+@Table(name = "subscriptions")
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Enumerated(value = EnumType.STRING)
+    private Type type;
+
+    @NotNull
+    private Date start_date;
+
+    @NotNull
+    private Date finish_date;
+}
---
+++ subscription-service/src/main/java/com/example/subscriptionservice/Domain/enums/Type.java
@@ -0,0 +1,7 @@
+package com.example.subscriptionservice.Domain.enums;
+
+public enum Type {
+    Premium,
+    Mid,
+    Classic,
+}
