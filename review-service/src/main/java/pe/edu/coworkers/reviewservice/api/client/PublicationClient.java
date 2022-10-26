e4a813caadb960ef5c4c6b8c4b12c317b0bd918f
Edward240402 <eduard.coar@gmail.com>
2022-10-25 23:33:02 -0500

    fix: update port

--- review-service/src/main/java/pe/edu/coworkers/reviewservice/api/client/PublicationClient.java
+++ review-service/src/main/java/pe/edu/coworkers/reviewservice/api/client/PublicationClient.java
@@ -4,10 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import pe.edu.coworkers.reviewservice.domain.model.model.Publication;
-
-import java.util.List;
 
 @FeignClient(name = "publication-service", decode404 = true, fallback = PublicationHystrixFallbackFactory.class)
 public interface PublicationClient {
