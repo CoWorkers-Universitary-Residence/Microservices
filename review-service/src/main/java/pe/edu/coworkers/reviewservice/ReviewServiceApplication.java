d4d612e3a904e51459f1a19d42e03cdc745f26a2
Edward240402 <eduard.coar@gmail.com>
2022-10-25 21:35:59 -0500

    feat: Update Eureka dependency

--- review-service/src/main/java/pe/edu/coworkers/reviewservice/ReviewServiceApplication.java
+++ review-service/src/main/java/pe/edu/coworkers/reviewservice/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package pe.edu.coworkers.reviewservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
