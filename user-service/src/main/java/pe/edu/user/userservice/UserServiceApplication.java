2ac301946c6dc7d0c61753a7fbee0ab15200e2d1
Paolo Cisneros <fercis2011@hotmail.com>
2022-10-26 01:37:50 -0500

    feat: update files

--- user-service/src/main/java/pe/edu/user/userservice/UserServiceApplication.java
+++ user-service/src/main/java/pe/edu/user/userservice/UserServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
